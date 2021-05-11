81538b7fe84e6170ae9e8007281d4205762e0e80
Romet Aidla <rometaidla@gmail.com>
2021-05-11 20:42:37 +0300

    Axis labels added to plots.

--- plotter.py
+++ plotter.py
@@ -11,13 +11,18 @@ def normalize_price(df):
 
 
 def plot(df, stock, save_figure=False):
+
     plt.figure(figsize=(15, 7))
+    plt.rcParams.update({'font.size': 14})
+
     stock_df = df[df['Stock'] == stock]
     normalized_price = normalize_price(stock_df)
     plt.plot(stock_df['Date'], stock_df['Sentiment'])
     plt.plot(stock_df['Date'], normalized_price)
     plt.legend(["Twitter sentiment", "Normalised stock price"])
     plt.title(f"{stock} sentiment")
+    plt.xlabel("Date")
+    plt.ylabel("Normalised price / Twitter sentiment")
     print(f"Correlation: {stock_df['Sentiment'].corr(stock_df['NormPrice']):0.2f}")
 
     if save_figure:
