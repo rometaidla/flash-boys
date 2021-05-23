76a10bb4530b558c78fa74116abe5d938e139d62
Romet Aidla <rometaidla@gmail.com>
2021-05-23 15:56:11 +0300

    Multiple stock support added to sentiment plotter.

--- plotter.py
+++ plotter.py
@@ -10,29 +10,34 @@ def normalize_price(df):
     return min_max_scaler.fit_transform(price)
 
 
-def plot(df, stock, factors, save_figure=False):
+def plot(df, stocks, factors, save_figure=False):
+    columns = 2
+    fig, axs = plt.subplots(3, columns, figsize=(20, 20), facecolor="white")
+    fig.autofmt_xdate()
+    axs = axs.reshape(-1)
+    for i, stock in enumerate(stocks):
+        ax = axs[i]
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.set_prop_cycle('color', ['#ffbd59', '#6ce5e8', '#b1b3b3', '#2c5696'])
 
-    fig = plt.figure(figsize=(10, 7), facecolor="white")
-    ax = plt.axes(frameon=True)
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.set_prop_cycle('color', ['#ffbd59', '#6ce5e8', '#b1b3b3', '#2c5696'])
-    plt.rcParams.update({'font.size': 14})
+        for f_idx, factor in enumerate(factors):
+            stock_df = df[(df['Stock'] == stock) & (df['Factor'] == factor)]
+            if len(stock_df):
+                normalized_price = normalize_price(stock_df)
+                #print(f"{factor} correlation: {stock_df['Sentiment'].corr(stock_df['NormPrice']):0.2f}")
+
+            if f_idx == 0:  # normalised price should be same for all factors, so just plot it once
+                ax.plot(stock_df['Date'], normalized_price)
+
+            ax.plot(stock_df['Date'], stock_df['Sentiment'])
+
+        ax.set_title(f"{stock} sentiment")
+        if i % columns == 0:
+            ax.set_ylabel("Normalised price / Twitter sentiment")
+
+    axs[1].legend(["Normalised price"] + factors)
     plt.xticks(rotation=45)
-
-    for i, factor in enumerate(factors):
-        stock_df = df[(df['Stock'] == stock) & (df['Factor'] == factor)]
-        normalized_price = normalize_price(stock_df)
-        if i == 0:  # normalised price should be same for all factors, so just plot it once
-            plt.plot(stock_df['Date'], normalized_price)
-
-        plt.plot(stock_df['Date'], stock_df['Sentiment'])
-        print(f"{factor } correlation: {stock_df['Sentiment'].corr(stock_df['NormPrice']):0.2f}")
-
-    plt.legend(["Normalised price"] + factors)
-    plt.title(f"{stock} sentiment")
-    plt.xlabel("Date")
-    plt.ylabel("Normalised price / Twitter sentiment")
 
     if save_figure:
         plots_folder = Path("plots")
