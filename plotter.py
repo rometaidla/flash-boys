260a0b05bc1b5ae1a7a35ccfd7a9b9108f1d1432
Romet Aidla <rometaidla@gmail.com>
2021-05-23 14:21:33 +0300

    Multiple factor support added to sentiment plotter.

--- plotter.py
+++ plotter.py
@@ -10,20 +10,29 @@ def normalize_price(df):
     return min_max_scaler.fit_transform(price)
 
 
-def plot(df, stock, save_figure=False):
+def plot(df, stock, factors, save_figure=False):
 
-    plt.figure(figsize=(15, 7))
+    fig = plt.figure(figsize=(10, 7), facecolor="white")
+    ax = plt.axes(frameon=True)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.set_prop_cycle('color', ['#ffbd59', '#6ce5e8', '#b1b3b3', '#2c5696'])
     plt.rcParams.update({'font.size': 14})
+    plt.xticks(rotation=45)
 
-    stock_df = df[df['Stock'] == stock]
-    normalized_price = normalize_price(stock_df)
-    plt.plot(stock_df['Date'], stock_df['Sentiment'])
-    plt.plot(stock_df['Date'], normalized_price)
-    plt.legend(["Twitter sentiment", "Normalised stock price"])
+    for i, factor in enumerate(factors):
+        stock_df = df[(df['Stock'] == stock) & (df['Factor'] == factor)]
+        normalized_price = normalize_price(stock_df)
+        if i == 0:  # normalised price should be same for all factors, so just plot it once
+            plt.plot(stock_df['Date'], normalized_price)
+
+        plt.plot(stock_df['Date'], stock_df['Sentiment'])
+        print(f"{factor } correlation: {stock_df['Sentiment'].corr(stock_df['NormPrice']):0.2f}")
+
+    plt.legend(["Normalised price"] + factors)
     plt.title(f"{stock} sentiment")
     plt.xlabel("Date")
     plt.ylabel("Normalised price / Twitter sentiment")
-    print(f"Correlation: {stock_df['Sentiment'].corr(stock_df['NormPrice']):0.2f}")
 
     if save_figure:
         plots_folder = Path("plots")
